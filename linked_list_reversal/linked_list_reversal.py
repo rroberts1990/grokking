85ebec84efaf74487d780fd81fb34045202e9f1e
Ross Roberts <ross123roberts@gmail.com>
2022-04-30 16:20:03 -0400

    reverse linked list

--- linked_list_reversal/linked_list_reversal.py
+++ linked_list_reversal/linked_list_reversal.py
@@ -12,6 +12,9 @@ class Node:
         rep += "null"
         return rep
 
+    def __repr__(self):
+        return f"Node({self.value})->{self.next}"
+
 class ReverseLinkedList:
 
     def reverse_linked_list(self, head):
@@ -24,3 +27,67 @@ class ReverseLinkedList:
             previous = current
             current = next_node
         return previous
+
+    def reverse_sub_list(self, head, p, q):
+        if p == q:
+            return head
+        current = head
+        previous = None
+        i = 0
+        while current is not None and i < p - 1:
+            previous = current
+            current = current.next
+            i += 1
+
+        node_before_p = previous
+        last_node_of_sublist = current
+
+        i = 0
+        while current is not None and i < q - p + 1:
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+            i += 1
+
+        if node_before_p is not None:
+            node_before_p.next = previous
+        else:
+            head = previous
+
+        last_node_of_sublist.next = current
+        return head
+
+    def reverse_every_k_sublist(self, head, k):
+        if k <= 1 or head is None:
+            return head
+
+        current = head
+        previous = None
+
+        while True:
+            last_node_of_prev_part = previous
+            last_node_of_sublist = current
+            next_node = None
+            i = 0
+            while current is not None and i < k:
+                next_node = current.next
+                current.next = previous
+                previous = current
+                current = next_node
+                i += 1
+
+            if last_node_of_prev_part is not None:
+                last_node_of_prev_part.next = previous
+            else:
+                head = previous
+
+                # connect with the next part
+            last_node_of_sublist.next = current
+
+            if current is None:
+                break
+            previous = last_node_of_sublist
+        return head
+
+
--- linked_list_reversal/test_linked_list_reversal.py
+++ linked_list_reversal/test_linked_list_reversal.py
@@ -20,3 +20,38 @@ def test_reverse_linked_list(example_list_1, example_result_1):
     actual = solver.reverse_linked_list(example_list_1)
     print(actual.print_list())
     assert actual.print_list() == example_result_1
+
+@pytest.fixture
+def example_result_2():
+    return "2->8->6->4->10->null"
+
+def test_reverse_sub_list(example_list_1, example_result_2):
+    p, q = 2, 4
+    solver = ReverseLinkedList()
+    actual = solver.reverse_sub_list(example_list_1, p, q)
+    print(actual.print_list())
+    assert actual.print_list() == example_result_2
+
+
+@pytest.fixture
+def example_list_2():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    head.next.next.next.next.next.next = Node(7)
+    head.next.next.next.next.next.next.next = Node(8)
+    return head
+
+@pytest.fixture
+def example_result_3():
+    return "3->2->1->6->5->4->8->7->null"
+
+def test_reverse_every_k_sublist(example_list_2, example_result_3):
+    k = 3
+    solver = ReverseLinkedList()
+    actual = solver.reverse_every_k_sublist(example_list_2, k)
+    print(actual.print_list())
+    assert actual.print_list() == example_result_3
