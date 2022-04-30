466e4dd8a0ab8a1b59df91041da2135902cfefa9
Ross Roberts <ross123roberts@gmail.com>
2022-04-30 12:44:35 -0400

    cyclic sort

--- cyclic_sort/cyclic_sort.py
+++ cyclic_sort/cyclic_sort.py
@@ -15,3 +15,119 @@ class CyclicSort:
                 i += 1
 
         return nums
+
+    def find_missing_number(self, nums):
+        i, n = 0, len(nums)
+        while i < n:
+            j = nums[i]
+            if j < n and j != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+
+        for i in range(n):
+            if nums[i] != i:
+                return i
+        return n
+
+    def find_all_missing_numbers(self, nums):
+        i = 0
+        missing_numbers = []
+        while i < len(nums):
+            j = nums[i] - 1
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                missing_numbers.append(i + 1)
+
+        return missing_numbers
+
+    def find_duplicate_number(self, nums):
+        i = 0
+
+        while i < len(nums):
+            if nums[i] != i + 1:
+                j = nums[i] - 1
+                if nums[i] != nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                else:
+                    return nums[i]
+            else:
+                i += 1
+
+    def find_all_duplicate_numbers(self, nums):
+        i = 0
+
+        while i < len(nums):
+            j = nums[i] - 1
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+        dupes = []
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                dupes.append(nums[i])
+        return dupes
+
+    def find_corrupt_pair(self, nums):
+        i = 0
+        while i < len(nums):
+            j = nums[i] - 1
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return [nums[i], i + 1]
+
+        return [-1, -1]
+
+    def find_smallest_missing_positive(self, nums):
+        i = 0
+
+        while i < len(nums):
+            j = nums[i] - 1
+            if nums[i] > 0 and nums[i] <= len(nums) and nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return i + 1
+        return len(nums) + 1
+
+    def find_first_k_missing(self, nums, k):
+        i = 0
+        missing = []
+        n = len(nums)
+
+        while i < n:
+            j = nums[i] - 1
+            if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
+
+        extra = set()
+        for i in range(n):
+            if len(missing) < k:
+                if nums[i] != i + 1:
+                    missing.append(i + 1)
+                    extra.add(nums[i])
+        i = 1
+        while len(missing) < k:
+            candidate = i + n
+            if candidate not in extra:
+                missing.append(candidate)
+            i += 1
+        return missing
+
+
+
+
--- cyclic_sort/test_cyclic_sort.py
+++ cyclic_sort/test_cyclic_sort.py
@@ -10,3 +10,68 @@ def test_cyclic_sort(nums, expected):
     actual = solver.cyclic_sort(nums)
     assert actual == expected
 
+
+@pytest.mark.parametrize('nums, expected',
+                         [([4, 0, 3, 1], 2),
+                          ([8, 3, 5, 2, 4, 6, 0, 1], 7)])
+def test_find_missing_number(nums, expected):
+    solver = CyclicSort()
+    actual = solver.find_missing_number(nums)
+    assert actual == expected
+
+
+@pytest.mark.parametrize('nums, expected',
+                         [([2, 3, 1, 8, 2, 3, 5, 1], [4, 6, 7]),
+                          ([2, 4, 1, 2], [3]),
+                          ([2, 3, 2, 1], [4])])
+def test_find_all_missing_numbers(nums, expected):
+    solver = CyclicSort()
+    actual = solver.find_all_missing_numbers(nums)
+    assert actual == expected
+
+
+@pytest.mark.parametrize('nums, expected',
+                         [([1, 4, 4, 3, 2], 4),
+                          ([2, 1, 3, 3, 5, 4], 3),
+                          ([2, 4, 1, 4, 4], 4)])
+def test_find_duplicate_numbers(nums, expected):
+    solver = CyclicSort()
+    actual = solver.find_duplicate_number(nums)
+    assert actual == expected
+
+
+@pytest.mark.parametrize('nums, expected',
+                         [([3, 4, 4, 5, 5], [5, 4]),
+                          ([5, 4, 7, 2, 3, 5, 3], [3, 5])])
+def test_find_all_duplicate_numbers(nums, expected):
+    solver = CyclicSort()
+    actual = solver.find_all_duplicate_numbers(nums)
+    assert actual == expected
+
+@pytest.mark.parametrize('nums, expected',
+                         [([3, 1, 2, 5, 2], [2, 4]),
+                          ([3, 1, 2, 3, 6, 4], [3, 5])])
+def test_find_corrupt_pairs(nums, expected):
+    solver = CyclicSort()
+    actual = solver.find_corrupt_pair(nums)
+    assert actual == expected
+
+
+@pytest.mark.parametrize('nums, expected',
+                         [([-3, 1, 5, 4, 2], 3),
+                          ([3, -2, 0, 1, 2], 4),
+                          ([3, 2, 5, 1], 4),
+                          ([33, 37, 5], 1)])
+def test_find_smalled_missing_positive(nums, expected):
+    solver = CyclicSort()
+    actual = solver.find_smallest_missing_positive(nums)
+    assert actual == expected
+
+@pytest.mark.parametrize('nums, k, expected',
+                         [([3, -1, 4, 5, 5], 3, [1, 2, 6]),
+                          ([2, 3, 4], 3, [1, 5, 6]),
+                          ([-2, -3, 4], 2, [1, 2])])
+def test_find_first_k_missing(nums, k, expected):
+    solver = CyclicSort()
+    actual = solver.find_first_k_missing(nums, k)
+    assert actual == expected
